7d77e0f824d561dcba6edbe50a0ea96e76383257
Daniel Deane <daniel.michael.deane@gmail.com>
2023-08-04 10:01:03 -0500

    still working on

---
+++ js/break_and_continue.js
@@ -0,0 +1,17 @@
+
+
+for (let i = 0; i < 49; i++) {
+
+    let i = (Math.floor(Math.random() * 50) + 1)
+    if (randomNumber % 2 === 1) ;
+}
+    console.log("Number to skip is; " + randomNumber);
+
+
+
+//
+// } else(i === 1);
+//           console.log(randomNumber + ' is odd');
+//         // } else {
+//         //  console.log("Yikes! Skipping " )}
+
