17065818d7f8afdbd37c0e6f36195d6259c5f216
Daniel Deane <daniel.michael.deane@gmail.com>
2023-08-07 12:28:52 -0500

    need to finish last one

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,17 +1,45 @@
-
-
-for (let i = 0; i < 49; i++) {
-
-    let i = (Math.floor(Math.random() * 50) + 1)
-    if (randomNumber % 2 === 1) ;
-}
-    console.log("Number to skip is; " + randomNumber);
-
-
-
 //
-// } else(i === 1);
-//           console.log(randomNumber + ' is odd');
-//         // } else {
-//         //  console.log("Yikes! Skipping " )}
+//
+//
+// let random = Math.floor((Math.random() * 50) + 2)
+//
+// console.log("Number to skip is: " + random);
+//
+//
+// for (let i = 1; i < 50; i++) {
+//
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     if (random === i){;
+//             console.log("Yikes! Your are skipping " + i);
+//     } else  {
+//         console.log("Here is an odd number: " + i )
+//     }
+//     if (i >= 49) {
+//         break;
+//     }
+// }
 
+let userNum;
+do{
+userNum=prompt("Please enter number 1-50")
+if (isEven)()){
+    alert("That is not an odd number")
+        continue
+    }
+if (userNum < 1 || userNum > 49){
+    alert("That number is out side of range")
+    continue;
+break;
+
+}while(true);
+console.log(userNum):
+    for (let i =0; i< 50: i++);{
+        if (isEven(i)){
+            continue;
+        }
+    if(userNum === i){
+    console.log("here is a wild number"){;
+    }
+}
---
+++ js/iterating.js
@@ -0,0 +1,59 @@
+(function(){
+    "use strict";
+    console.log("Hello");
+    /**
+     * TODO:
+     * Create an array of 4 people's names and store it in a variable called
+     * 'names'.
+     */
+// let nameOf = [ "Phillip", "Joe", "Steve" , "Jack"];
+    /**
+     * TODO:
+     * Create a log statement that will log the number of elements in the names
+     * array.
+     */
+// console.log(nameOf.length)
+    /**
+     * TODO:
+     * Create log statements that will print each of the names individually by
+     * accessing each element's index.
+     */
+    // console.log(nameOf[0])
+    // console.log(nameOf[1])
+    // console.log(nameOf[2])
+    // console.log(nameOf[3])
+
+    /**
+     * TODO:
+     * Write some code that uses a for loop to log every item in the names
+     * array.
+     */
+//     for (let i = 0; i < nameOf.length; i++){
+//     console.log("The name at index " + i + " is " + nameOf[i]);
+// }
+    /**
+     * TODO:
+     * Refactor your above code to use a `forEach` loop
+     */
+// let nameOf = [ "Phillip", "Joe", "Steve" , "Jack"]
+//     nameOf.forEach(function (name) {
+//         console.log("Meet " + name + ", he is a great guy." );
+//     });
+
+    /**
+     * TODO:
+     * Create the following three functions, each will accept an array and
+     * return an an element from it
+     * - first: returns the first item in the array
+     * - second: returns the second item in the array
+     * - last: returns the last item in the array
+     *
+     * Example:
+     *  > first([1, 2, 3, 4, 5]) // returns 1
+     *  > second([1, 2, 3, 4, 5]) // returns 2
+     *  > last([1, 2, 3, 4, 5]) // return 5
+     */
+    numbersOF.forEach(i)= [1, 2, 3, 4, 5]
+
+    let
+})();
