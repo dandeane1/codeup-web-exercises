615e89b3b6d2404e0b68acbedacc823f27c154bc
Daniel Deane <daniel.michael.deane@gmail.com>
2023-08-15 12:02:34 -0500

    css intro added

---
+++ js/warmup.js
@@ -0,0 +1,15 @@
+
+// Warmup for today:
+// Create a function called logUser. This function will accept a string argument and return a message that says “stringArgument has logged in for the day”. Console.log your results to verify the message.
+// console.log(logUser(“ken2cool”)) // “ken2cool has logged in for the day.”
+// 	Bonus:
+// I. Refactor your message to use template strings.
+// Ii. Add validation to make sure the argument passed to the function is a string datatype only.
+
+function logUser(string){
+    return (string) + "has logged in for the day.";
+
+    }
+console.log(logUser + "has logged in for the day.")
+// “ken2cool has logged in for the day.”
+    (logUser(ken))
