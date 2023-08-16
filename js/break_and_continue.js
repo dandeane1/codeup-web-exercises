d068e6a3a166c96cacbbbed2563a9a3d8411785c
Daniel Deane <daniel.michael.deane@gmail.com>
2023-08-15 23:05:02 -0500

    found syntax error now complete

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,43 +1,49 @@
-//
-//
-//
-// let random = Math.floor((Math.random() * 50) + 2)
-//
-// console.log("Number to skip is: " + random);
-//
-//
-// for (let i = 1; i < 50; i++) {
-//
-//     if (i % 2 === 0) {
-//         continue;
-//     }
-//     if (random === i){;
-//             console.log("Yikes! Your are skipping " + i);
-//     } else  {
-//         console.log("Here is an odd number: " + i )
-//     }
-//     if (i >= 49) {
-//         break;
-//     }
-// }
+
+
+
+let random = Math.floor((Math.random() * 50) + 2)
+
+console.log("Number to skip is: " + random);
+
+
+for (let i = 1; i < 50; i++) {
+
+    if (i % 2 === 0) {
+        continue;
+    }
+    if (random === i){;
+            console.log("Yikes! Your are skipping " + i);
+    } else  {
+        console.log("Here is an odd number: " + i )
+    }
+    if (i >= 49) {
+        break;
+    }
+}
 
 let userNum;
 do {
-    userNum = prompt("Please enter number 1-50")
-    if (isEven()) {
-        alert("That is not an odd number")
-        continue
-    }
+    userNum = parseInt(prompt("Please enter number 1-50"));
+if (!isFinite(userNum))  {
+    alert("Please enter a valid number.");
+        continue;
+}
     if (userNum < 1 || userNum > 49) {
         alert("That number is out side of range")
         continue;
-        break;
+    }
+    if (userNum % 2=== 0){
+        alert("That is not an odd number.")
+        continue
+    }
+            break;
+
 
     }
     while (true) ;
-    console.log(userNum);
+    console.log(`User's odd number: ${userNum}`);
     for (let i = 0; i < 50; i++) {
-        if (isEven(i)) {
+        if (i % 2 === 0) {
             continue;
         }
         if (userNum === i) {
