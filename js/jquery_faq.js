a679809180a23b78fdca8e7440c870918b60e379
Daniel Deane <daniel.michael.deane@gmail.com>
2023-10-09 15:35:48 -0500

    fixed functionablity

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,4 +1,22 @@
-$( "dd" ).addClass( "invisible" );
-    $('dd').click(function (event) {
-        $('this').toggleClass('invisible');
-    });
+// Exercise 1
+$(".highlight-me").css("background-color", "yellow");
+
+// Exercise 3
+$("h1").click(function() {
+    $(this).css("background-color", "blue");
+});
+
+// Exercise 4
+$("p").dblclick(function() {
+    $(this).css("font-size", "18px");
+});
+
+// Exercise 5
+$("li").hover(
+    function() {
+        $(this).css("color", "red");
+    },
+    function() {
+        $(this).css("color", "black");
+    }
+);
